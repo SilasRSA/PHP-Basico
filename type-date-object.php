38be630b57ae8a701b5055a961a492ad211c90d2
Silas Rodrigues <79548192+SilasRSA@users.noreply.github.com>
2022-04-10 12:40:28 -0300

    Update type-date-object.php

--- type-date-object.php
+++ type-date-object.php
@@ -24,12 +24,24 @@
         private $color;
         private $model;
         private $anoFabricacao;
-   
+        
+        /*
+            Criacao do contrutor para a classe Car. 
+            Ele recebe tres parametros e os atribui
+            as tres propriedades do objeto.
+        */
+        
         public function Car ($color, $model, $anoFabricacao){
             $this->color = $color;
             $this->model = $model;
             $this->anoFabricacao = $anoFabricacao;
         }
+        
+        /*
+            Criacao do metodo myCarMessage que 
+            retorna uma menssagem sobre as caracteristicas
+            do objeto Car.
+        */
 
         public function myCarMessage(){
             return "My Car is a " . $this->color . " " . $this->model . " " . $this->anoFabricacao. "!";
