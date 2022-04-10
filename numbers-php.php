add937cf2ea671774bf5c9b73e8bdd468ce8cd35
Silas Rodrigues <79548192+SilasRSA@users.noreply.github.com>
2022-04-10 16:09:36 -0300

    Add files via upload

--- numbers-php.php
+++ numbers-php.php
@@ -58,4 +58,27 @@
     echo PHP_INT_MAX; // echo retorna 9223372036854775807 que e o maior inteiro suportado
     echo PHP_INT_MIN; // echo retorna -9223372036854775808 que o menor inteiro suportado
     echo PHP_INT_SIZE; // echo retorna 8 que e o tamanho do inteiro em bytes
+
+    /*
+        Um float e um numero com ponto decimal ou na forma exponencial.
+        O PHP tem as seguintes constantes predefinidas para floats:
+        
+            - O maior float suportado: PHP_FLOAT_MAX
+            - O menor float POSITIVO suportado: PHP_FLOAT_MIN
+            - O menor float NEGATIVO suportado: -PHP_FLOAT_MAX
+            - O numero de digitos decimais que podem ser arredondados
+              para um float e vice-versa sem perder a precisao: PHP_FLOAT_DIG
+            - O menor numero positivo representavel x, de modo que
+              x + 1.0 = 1.0: PHP_FLOAT_EPSILON  
+
+    */
+
+    echo PHP_FLOAT_MAX; // echo retorna 1.7976931348623E+308 que o maior float representavel
+    echo PHP_FLOAT_MIN; // echo retorna 2.2250738585072E-308 que o menor float POSITIVO representavel
+    echo -PHP_FLOAT_MAX; // echo retorna -1.7976931348623E+308 que o menor float NEGATIVO representavel
+    echo PHP_FLOAT_DIG; // echo retorna 15 que o numero maximo de digitos decimais que podem ser arredondados mantendo a precisao
+    echo PHP_FLOAT_EPSILON; //echo retorna 2.2204460492503E-16
+                        
+                      
+
 ?>
